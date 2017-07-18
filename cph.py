af177abd8f37e0d17f0b36aad564a74a9642fa59
c3phalexin <0xc3phalex1n@gmail.com>
2017-07-18 20:50:15 +0430

    Update cph.py

--- cph.py
+++ cph.py
@@ -3,17 +3,15 @@
 # Cephalexin Shellcoder is a script for extracting shellcode from a binary.
 # Written by Milad Kahsari Alhadi
 
-
 import sys
 import os
-import re
 
 def Header():
         print("\n")
         print("=============================================================")
         print("\n\033[93m\033[1mCephalexin Shellcoder extracts shellcode (machine code).\033[0m")
         print("\033[93m\033[1mScript is written by Milad Kahsari Alhadi.\033[0m\n")
-        print("\t\033[90m\033[1mCurrent Version: v1.0.2\033[0m\n")
+        print("\t\033[90m\033[1mCurrent Version: v2.1.2\033[0m\n")
         print("=============================================================")
 
 def ObjDumpOutput(_arg_name):
@@ -21,6 +19,30 @@ def ObjDumpOutput(_arg_name):
         output = os.popen("objdump -d -M intel " + str(_arg_name)).read()
         for line in output.split('\n'):
                 print('\t', line)
+
+def ScanStringSysCalls(_arg_string):
+        totalSysCalls = 0
+        print("\033[92m\033[1mSystem Calls Detected:\033[0m\n")
+
+        for pos, check in enumerate(_arg_string):
+                if check == '\\xcd':
+                        print("\tSystem calls at {} index detected".format(pos))
+                        totalSysCalls += 1
+
+        print("\n\t\033[95m\033[1mTotal system calls in the shellcode: {}\033[0m\n".format(totalSysCalls))
+
+
+def ScanStringNullByte(_arg_string):
+        totalNullByte = 0
+        print("\033[91m\033[1mNull Bytes Detected:\033[0m\n")
+        
+        for pos, check in enumerate(_arg_string):
+                if check == '\\x00':
+                        print("\tNull byte at {} index detected".format(pos))
+                        totalNullByte += 1
+
+        print("\n\t\033[95m\033[1mTotal null bytes in the shellcode: {}\033[0m\n".format(totalNullByte))
+
 
 def ExtractShellcode(_arg_name):
         g1 = "grep '[0-9a-f]:'"
@@ -43,10 +65,12 @@ def ExtractShellcode(_arg_name):
         result = [r'\x' + result[i:i + 2] for i in range(0, length, 2)]
 	
         ObjDumpOutput(_arg_name)
-        print("\033[91m\033[1mExtracted Shellcode:\033[0m\n")
-        print("\t {}". format(''.join(result)))
+        print("\033[101m\033[1mExtracted Shellcode:\033[0m\n")
+        print("\t {}".format(''.join(result)))
         print("\n")
 
+        ScanStringNullByte(result)
+        ScanStringSysCalls(result)
 
 def EntryPoint():
         if len(sys.argv) != 2:
