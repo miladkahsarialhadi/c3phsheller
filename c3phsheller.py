e3c434b3c813e898bbb83fb47ad22418ca1aff68
c3phalexin <0xc3phalex1n@gmail.com>
2017-07-18 14:11:18 +0430

    Update c3phsheller.py

--- c3phsheller.py
+++ c3phsheller.py
@@ -6,8 +6,22 @@
 
 import sys
 import os
+import re
 
-def extract(name):
+def Header():
+        print("\n")
+        print("=============================================================")
+        print("\n\033[93m\033[1mCephalexin Sheller extracts shellcode (machine code).\033[0m")
+        print("\033[93m\033[1mScript is written by Milad Kahsari Alhadi.\033[0m\n")
+        print("=============================================================")
+
+def ObjDumpOutput(_arg_name):
+        print("\n\033[96m\033[1mObjdump Output:\033[0m")
+        output = os.popen("objdump -d -M intel " + str(_arg_name)).read()
+        for line in output.split('\n'):
+                print('\t', line)
+
+def ExtractShellcode(_arg_name):
         g1 = "grep '[0-9a-f]:'"
         g2 = "grep -v 'file'"
         g3 = "cut -f2 -d:"
@@ -20,26 +34,27 @@ def extract(name):
         g10 = "sed 's/$/\"/g'"
         gg = "tr '\\t' ' '"
 
-        cmd1 = "objdump -d "
-        cmd2 = str(name) + "|" + g1 + "|" + g2 + "|" + g3 + "|" + g4 + "|" + g5 + "|" + gg + "|" + g6 + "|" + g7 + "|" + g8 + "|" + g9 + "|" + g10
+        objdump = "objdump -d " + str(_arg_name) + "|" + g1 + "|" + g2 + "|" + g3 + "|" + g4 + "|" + g5 + "|" + gg + "|" + g6 + "|" + g7 + "|" + g8 + "|" + g9 + "|" + g10
+        result = os.popen(objdump).read()
+        result = result.replace('"','')
 
-        cmd3 = str(cmd1) + str(cmd2)
-        res = os.popen(cmd3).read()
-        s = res.replace('"','')
+        length = len(result) - 1
+        result = [r'\x' + result[i:i + 2] for i in range(0, length, 2)]
+	
+        ObjDumpOutput(_arg_name)
+        print("\033[91m\033[1mShellcode:\033[0m\n")
+        print("\t {}". format(''.join(result)))
+        print("\n")
 
-        length = len(s) - 1
-        a = [r'\x' + s[i:i + 2] for i in range(0, length, 2)]
 
-        print(''.join(a))
-
-
-def main():
+def EntryPoint():
         if len(sys.argv) != 2:
-                print("Usage: ./c3phsheller shellcode")
+                print("\n\t\033[96mUsage: ./c3phsheller [binary]\033[0m\n\n")
 
         else:
-                extract(sys.argv[1])
+                ExtractShellcode(sys.argv[1])
+		
 
 if __name__ == "__main__":
-        print("C3phalex1n Sheller - Written by Milad Kahsari Alhadi")
-        main()  
+        Header()
+        EntryPoint()  
